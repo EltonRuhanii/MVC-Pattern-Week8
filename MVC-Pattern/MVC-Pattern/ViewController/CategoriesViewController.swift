fef6870302f28fae907e205b64204bbdac39d0e4
Elton Ruhani <134723231+EltonRuhanii@users.noreply.github.com>
2024-01-23 16:41:05 +0100

    Added joke categories

---
+++ MVC-Pattern/MVC-Pattern/ViewController/CategoriesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoriesViewController.swift
+//  MVC-Pattern
+//
+//  Created by Kin+Carta on 23.1.24.
+//
+
+import UIKit
+
+class CategoriesViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
