27733d58268b520beba6e1f43e96a98b08455bdd
Elton Ruhani <134723231+EltonRuhanii@users.noreply.github.com>
2024-01-23 09:51:53 +0100

    Created XCode file

---
+++ MVC-Pattern/MVC-Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-Pattern
+//
+//  Created by Kin+Carta on 23.1.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
